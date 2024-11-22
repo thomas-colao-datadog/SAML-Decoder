62d366ab0c699a796f09a482abd8d08ab7e7ebac
thomas-colao-datadog <thomas.colao@datadoghq.com>
2024-11-22 15:21:11 -0500

    add decode.py

---
+++ decode.py
@@ -0,0 +1,26 @@
+import base64
+import xml.etree.ElementTree as ET
+
+def parseAttributes():
+    tree = ET.parse('decoded_assertion.xml')
+    root = tree.getroot()
+    attributes = []
+    for Attribute in root.iter('{urn:oasis:names:tc:SAML:2.0:assertion}Attribute'):
+        attributeName = Attribute.attrib
+        attributeValue = Attribute.find("{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue").text
+        attributes.append((attributeName, attributeValue))
+    return attributes
+
+def decode(encoded_path = 'encoded_assertion.txt', decoded_path = 'decoded_assertion.txt'):
+    f = open(encoded_path, 'r')
+    encoded_assertion = f.read()
+    f.close()
+    decoded_assertion = base64.b64decode(encoded_assertion).decode('utf-8')
+    f = open(decoded_path, 'w')
+    f.write(str(decoded_assertion))
+    f.close()
+
+decode()
+attributes = parseAttributes()
+for attribute in attributes:
+    print(attribute)
