6df3db5676d5bf1407fec359a4a14e9a515d96aa
thomas-colao-datadog <thomas.colao@datadoghq.com>
2024-11-24 17:22:56 -0500

    added basic error handling

--- decode.py
+++ decode.py
@@ -33,7 +33,6 @@ class Assertion:
     def parse_attributes_azure(self):
         attributes = []
         for Attribute in self.xml_root.iter('{urn:oasis:names:tc:SAML:2.0:assertion}Attribute'):
-            print(Attribute.tag)
             attributeName = str(Attribute.attrib)
             attributeName = re.search("Name': '(.*?)'", attributeName)[1]
             attributeValue = Attribute.find("{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue").text
@@ -56,11 +55,14 @@ class Assertion:
     
     def __str__(self):
         output = ""
-        output += "Identity Provider: " + self.identity_provider + "\n"
-        output += "Attributes:"
-        for i in range(0, len(self.attributes)):
-            output += "\n" + str(self.attributes[i][0]) + ": "
-            output += str(self.attributes[i][1])
+        if self.identity_provider == "N/A":
+            output = "Error: Unable to decode SAML Assertion"
+        else:
+            output += "Identity Provider: " + self.identity_provider + "\n"
+            output += "Attributes:"
+            for i in range(0, len(self.attributes)):
+                output += "\n" + str(self.attributes[i][0]) + ": "
+                output += str(self.attributes[i][1])
         return output
     
 if __name__ == "__main__":
