3fea4be528cb5e08e378cfae3e38973a773025d1
thomas-colao-datadog <thomas.colao@datadoghq.com>
2024-11-25 09:35:45 -0500

    refactored parse_attributes

--- decode.py
+++ decode.py
@@ -9,9 +9,9 @@ class Assertion:
         self.xml_root = ET.fromstring(self.decoded_assertion)
         self.identity_provider = self.parse_idp()
         if self.identity_provider == "Okta":
-            self.attributes = self.parse_attributes_okta()
+            self.attributes = self.parse_attributes("def:(.*?)'")
         if self.identity_provider == "Azure":
-            self.attributes = self.parse_attributes_azure()
+            self.attributes = self.parse_attributes("Name': '(.*?)'")
         pass
 
     def decode(self):
@@ -21,20 +21,11 @@ class Assertion:
         return str(base64.b64decode(encoded_assertion).decode('utf-8'))
         
     
-    def parse_attributes_okta(self):
+    def parse_attributes(self, regex):
         attributes = []
         for Attribute in self.xml_root.iter('{urn:oasis:names:tc:SAML:2.0:assertion}Attribute'):
             attributeName = str(Attribute.attrib)
-            attributeName = re.search("def:(.*?)'", attributeName)[1]
-            attributeValue = Attribute.find("{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue").text
-            attributes.append((attributeName, attributeValue))
-        return attributes
-    
-    def parse_attributes_azure(self):
-        attributes = []
-        for Attribute in self.xml_root.iter('{urn:oasis:names:tc:SAML:2.0:assertion}Attribute'):
-            attributeName = str(Attribute.attrib)
-            attributeName = re.search("Name': '(.*?)'", attributeName)[1]
+            attributeName = re.search(regex, attributeName)[1]
             attributeValue = Attribute.find("{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue").text
             attributes.append((attributeName, attributeValue))
         return attributes
